fb520a2cfeb6204939392a16daa820fdb96a021d
Nate <stwak92@gmail.com>
2020-02-18 07:56:28 -0700

    Skeleton Op Mode
    Learning the Basics

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkeletonOpMode.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="Skeleton Op Mode", group="TeleOp")
+public class SkeletonOpMode extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
